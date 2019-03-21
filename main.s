c608b09bc3dd7a81e4b60df020511ef719839c05
Calvin Ulsh <cju025@gmail.com>
2019-03-20 21:11:24 -0700

    Updates
    Part1.1 - Complete
    Part1.2 - Not Tested
    Part1.3 - Near Complete

--- main.s
+++ main.s
@@ -1,43 +1,30 @@
-# Name: Calvin Ulsh
-# Project 3 Part 1
-
 .text
-.equ input1       # part 1.1
-.equ input2       # part 1.1
-.equ A            # part 1.2
-.equ B            # part 1.2
-.equ C            # part 1.2
-.equ N            # part 1.3
-
-
 .global _start            # Part 1 ~TEST ALL~
 _start:
-  movia r2, INPUT1
-  movia r3, INPUT2
-  call  sum_two
-  movia r2, VAR_N
-
-  call  fibonacci
-
-
-
+    movia sp, 0x03ffffff
+    movia r2, INPUT1
+    movia r3, INPUT2
+    ldw   r4, 0(r2)
+    ldw   r5, 0(r3)
+    call  sum_two
+    movia r4, N
+    call  fib
 .global op_two            # Part 1.2 ~TWO INPUT TEST OPERATION~
 op_two:
-  ldw   r4, 0(r2)
-  ldw   r5, 0(r3)
-  multi r6, r4, r5
-  ret
-
+    ldw   r4, 0(r2)
+    ldw   r5, 0(r3)
+    mul   r6, r4, r5
+    ret
 .data
 INPUT1:
-  .byte 8
+  .word 0x8
 INPUT2:
-  .byte 6
-VAR_A:
-  .word 0x00000000
-VAR_B:
-  .word 0x00000000
-VAR_C:
-  .word 0x00000000
-VAR_N:
-  .byte 1
+  .word 0x6
+A:
+  .word 0x1
+B:
+  .word 0x2
+C:
+  .word 0x3
+N:
+  .word 0x7
--- part1.s
+++ part1.s
@@ -1,41 +1,70 @@
-; Name: Calvin Ulsh
-; Project 3 Part 1
-
 .text
-.global sum_two       ; Part 1.1 ~SUM~
+.global sum_two             # Part 1.1 ~SUM~
 sum_two:
-  ldw   r4, 0(r2)
-  ldw   r5, 0(r3)
   add   r6, r4, r5
   ret
 
-.global op_three      ; Part 1.2 ~THREE INPUT OPERATION~
+
+.global op_three            # Part 1.2 ~THREE INPUT OPERATION~
 op_three:
-  movia r2, VAR_A
-  movia r3, VAR_B
+  movia r2, A
+  movia r3, B
   call  op_two
-  movia r2, VAR_C
+  movia r2, C
   mov   r3, r6
   call  op_two
   ret
 
-.global fibonacci     ; Part 1.3 ~FIBONACCI~
-fibonacci:            ; Fibonacci(N - 1) + Fibonacci(N - 2)
-  ;Set Up Stack Memory
-  br LOOP
-LOOP:
-  ; Recall The New N Value From Stack Memory
-  ldb   r3, 0(r2)
-  subi  r4, r3, 1
-  subi  r5, r4, 1
-Minus1:                 ; Set Up Fibonacci(N - 1)
-  dec   r4
-  ; Save r4 Value To Stack Memory
-  bne   r4, r0, LOOP    ; Break To Fibonacci(N - 1)
-Minus2:                 ; Set Up Fibonacci(N - 2) After Fibonacci(N - 1) Branch Sequence
-  dec   r5
-  ; Save r5 Value To Stack Memory
-  bne   r5, r0, LOOP    ; Break To Fibonacci(N - 2)
-DONE:
-  ; Add Minus1 and Minus2 Value
-call Done
+
+.global fibonacci           # Part 1.3 ~FIBONACCI~
+fibonacci:                  # fibonacci(N - 1) + fibonacci(N - 2)
+  	addi	sp, sp, -8		    # Entry code
+		stw   ra, 0(sp)
+		stw		fp, 4(sp)
+		add		fp, sp,   zero		# End of entry code
+		ldw   r4, 8(fp)			    # r4 holds the argument n
+		slli	r3, r4,   2       # Compare n with 2. if r4 < 2 ...
+		beq		r3, zero, over    # ... skip the next two instructions
+		addi	r2, zero, 1       # n < 2. We're done with the recursion
+		call	exit				      # Jump to exit code
+over: 	                    # n >= 2
+    # Calculate fibonacci(n - 1)
+		addi	r4, r4,   -1		  # Calculate n - 1
+		addi	sp, sp,   -4		  # Allocate space for arguments
+		stw   r4, 0(sp)			    # n - 1 is our argument
+		call	fibonacci					# Call the fib procedure
+		addi	sp, sp,   4       # Pop off the argument
+		add		r3, r2,   zero		# r2 holds the result of fibonacci(n - 1). Put the result into r3
+    # Calculate fibonacci(n - 2)
+		ldw		r4, 8(fp)         # $r4 holds the argument n
+		addi	r4, r4,   -2      # Calculate n - 2
+		addi	sp, sp,   -4      # Allocate space for saved register
+		stw   r3, 0(sp)         # Save r3 (the result of fibonacci(n - 1))
+		addi	sp, sp,   -4      # Allocate space for arguments
+		stw   r4, 0(sp)         # n - 2 is our argument
+		call	fibonacci					# Call the fibonacci procedure
+		addi	sp, sp,   4       # Pop off the argument
+		ldw   r3, 0(sp)         # Restore r3 (the result of fibonacci(n - 1))
+		addi	sp, sp,   4       # Deallocate space for saved register
+		add		r2, r3,   r2      # r2 holds the result of fibonacci(n - 2). Result is fibonacci(n - 1) + fibonacci(n - 2)
+exit:
+		ldw		ra, 0(sp)         # Exit code
+		ldw		fp, 4(sp)
+		addi	sp, sp,   8
+		ret                     # End of exit code
+
+.data
+INPUT1:
+  .word 0x8
+INPUT2:
+  .word 0x6
+A:
+  .word 0x1
+B:
+  .word 0x2
+C:
+  .word 0x3
+N:
+  .word 0x7
+
+.end
