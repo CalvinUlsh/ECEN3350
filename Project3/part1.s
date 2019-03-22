b6ba64cfa523802e69f4eac620044ae56dd3584f
Calvin Ulsh <cju025@gmail.com>
2019-03-22 13:50:38 -0700

    finished

--- Project3/part1.s
+++ Project3/part1.s
@@ -1,70 +1,66 @@
+# Course:		ECEN 3350
+# Assignment:	Project 3.1
+# Name:			Calvin Ulsh
+
 .text
-.global sum_two             # Part 1.1 ~SUM~
+.global sum_two             		# Part 1.1 Calling Convention ~SUM~
 sum_two:
-  add   r6, r4, r5
-  ret
+	add   r3, r4, r5
+	ret
 
-
-.global op_three            # Part 1.2 ~THREE INPUT OPERATION~
+.global op_three            		# Part 1.2 Saving Registers ~THREE INPUT OPERATION~
 op_three:
-  movia r2, A
-  movia r3, B
-  call  op_two
-  movia r2, C
-  mov   r3, r6
-  call  op_two
-  ret
+	subi	sp, sp, 8				# push return address to stack - allocate
+	stw		ra, 4(sp)				# return address
+	stw		fp, 0(sp)				# frame pointer
+	call	op_two					# call two input function
+	ldw		ra, 4(sp)				# pop return address from stack
+	ldw		fp, 0(sp)				# pop frame pointer from stack
+	addi 	sp, sp, 8				# deallocate
+	mov		r4, r2					# set return value from op_two as new argument
+	mov		r5, r6
+	call	op_two					# call to compute the new argument
+	ret
 
+.global fibonacci               	# Part 1.3 Recursive Functions ~FIBONACCI~
+fibonacci:                      	# fibonacci(N - 1) + fibonacci(N - 2)
+  	subi	sp,	sp,	12		    	# stack frame pointer
+	stw		ra,	8(sp)				# store return address
+	stw		fp,	4(sp)				# store frame pointer
+	addi	fp,	sp,	4				# end of entry code
+	cmpeqi	r15,r4, 1          		# compare n with 1
+	beq		r15,r0,	one        		# skip to one if true
+	beq		r4, r0,	zero           	# skip to zero if r4 = 0 (n = 0)
+	stw		r4,-4(fp)				# store the last index value
+	subi	r4, r4, 1				# decrease n by 1
+	call	fibonacci				# recurse to top fibonacci and start again with n-1 value
+	ldw		r4,-4(fp)				# pop off r4 value
+	add		r2, r9, r10				# add r9 and r10, store in r2. r2 is the sum
+	mov		r9, r10					# move r10 (n - 1) to r9 (n - 2)			
+	mov		r10,r2					# and move r2 (sum) to r10 (n - 1)
+	br		stop					# r2 is our new (n - 1), r10 is our new (n - 2). break to stop and repeat.
+one: 	  	                      	# n = 1
+	movi	r9,	0					# first fibonacci sequence number
+	movi	r10,1					# second fibonacci sequence number
+	movi	r2, 1					# third fibonacci sequence number (first sum)
+	br		stop					# ready for next term...
+zero:								# n = 0
+	movi	r2, 0					# first fibonacci sequence number
+stop:
+	ldw		fp,	4(sp)				# restore fp
+	ldw		ra,	8(sp)				# restore ra
+	addi	sp, sp, 12				# deallocate stack space
+	ret
 
-.global fibonacci           # Part 1.3 ~FIBONACCI~
-fibonacci:                  # fibonacci(N - 1) + fibonacci(N - 2)
-  	addi	sp, sp, -8		    # Entry code
-		stw   ra, 0(sp)
-		stw		fp, 4(sp)
-		add		fp, sp,   zero		# End of entry code
-		ldw   r4, 8(fp)			    # r4 holds the argument n
-		slli	r3, r4,   2       # Compare n with 2. if r4 < 2 ...
-		beq		r3, zero, over    # ... skip the next two instructions
-		addi	r2, zero, 1       # n < 2. We're done with the recursion
-		call	exit				      # Jump to exit code
-over: 	                    # n >= 2
-    # Calculate fibonacci(n - 1)
-		addi	r4, r4,   -1		  # Calculate n - 1
-		addi	sp, sp,   -4		  # Allocate space for arguments
-		stw   r4, 0(sp)			    # n - 1 is our argument
-		call	fibonacci					# Call the fib procedure
-		addi	sp, sp,   4       # Pop off the argument
-		add		r3, r2,   zero		# r2 holds the result of fibonacci(n - 1). Put the result into r3
-    # Calculate fibonacci(n - 2)
-		ldw		r4, 8(fp)         # $r4 holds the argument n
-		addi	r4, r4,   -2      # Calculate n - 2
-		addi	sp, sp,   -4      # Allocate space for saved register
-		stw   r3, 0(sp)         # Save r3 (the result of fibonacci(n - 1))
-		addi	sp, sp,   -4      # Allocate space for arguments
-		stw   r4, 0(sp)         # n - 2 is our argument
-		call	fibonacci					# Call the fibonacci procedure
-		addi	sp, sp,   4       # Pop off the argument
-		ldw   r3, 0(sp)         # Restore r3 (the result of fibonacci(n - 1))
-		addi	sp, sp,   4       # Deallocate space for saved register
-		add		r2, r3,   r2      # r2 holds the result of fibonacci(n - 2). Result is fibonacci(n - 1) + fibonacci(n - 2)
-exit:
-		ldw		ra, 0(sp)         # Exit code
-		ldw		fp, 4(sp)
-		addi	sp, sp,   8
-		ret                     # End of exit code
+#.data
+#IN1:		.word 0x8
+#IN2:		.word 0x6
+#A:			.word 0x1
+#C:      	.word 0x3
+#N:      	.word 0x7
+#.end
 
-.data
-INPUT1:
-  .word 0x8
-INPUT2:
-  .word 0x6
-A:
-  .word 0x1
-B:
-  .word 0x2
-C:
-  .word 0x3
-N:
-  .word 0x7
-
-.end
+# r2 and r3			return values
+# r4 - r7			register arguments
+# r8 - r15			caller-saved general purpose registers
+# r16 - r23			callee-saved general purpose registers
